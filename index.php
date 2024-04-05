655a13626f00f42f0a47cf0b3440d11e8cb02bf2
devPxT <gabrielkauasantos11@gmail.com>
2024-04-05 08:17:12 -0300

    test cards idea for home

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Home</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
 <style>
     .buttons {
         display: flex;
@@ -61,13 +63,23 @@
         </div>
     </nav>
     
-    <div class="container">
+    <div class="container mt-4">
         <div class="row">
-            <div class="col-lg-6 col-sm-12">
-                <div class="button">CADASTRO</div>
+            <div class="col-lg-4 col-sm-12">
+                <div class="card" style="width: 18rem;">
+                    <i class="bi bi-box" style="font-size: 100px; margin: auto;"></i>
+                    <div class="card-body" style="margin: auto">
+                        <p class="card-text">CADASTROS</p>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-6 col-sm-12">
-                <div class="button">VENDAS</div>
+            <div class="col-lg-4 col-sm-12">
+                <div class="card" style="width: 18rem;">
+                    <i class="bi bi-tags" style="font-size: 100px; margin: auto;"></i>
+                    <div class="card-body" style="margin: auto">
+                        <p class="card-text">VENDAS</p>
+                    </div>
+                </div>
             </div>
             <div class="col-lg-6 col-sm-12">
                 <div class="button">ESTOQUE</div>

2d20eb567e58429ae4a921dd8568b0272536b562
devPxT <gabrielkauasantos11@gmail.com>
2024-04-07 12:20:06 -0300

    buttons in home -to change

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bungee+Spice&display=swap" rel="stylesheet">
+
 <style>
     .buttons {
         display: flex;
@@ -22,6 +26,22 @@
         background-color: #f0f0f0;
         border: 1px solid #ccc;
         cursor: pointer;
+    }
+    .card-body {
+        margin: 0px auto;
+        padding: 0px;
+    }
+    .card-body .card-text {
+        /* font-family: "Bungee Spice", sans-serif; */
+        font-weight: 900;
+        font-size: 30px;
+    }
+    .container-fluid .row .button-container {
+        display: flex;
+        justify-content: center;
+    }
+    .container-fluid .row {
+        justify-content: center;
     }
 </style>
 </head>
@@ -63,35 +83,54 @@
         </div>
     </nav>
     
-    <div class="container mt-4">
+    <div class="container-fluid mt-3">
         <div class="row">
-            <div class="col-lg-4 col-sm-12">
+            <div class="col-lg-4 col-md-5 col-sm-6 col-xs-12 button-container mb-4">
                 <div class="card" style="width: 18rem;">
-                    <i class="bi bi-box" style="font-size: 100px; margin: auto;"></i>
-                    <div class="card-body" style="margin: auto">
+                    <i class="bi bi-box" style="font-size: 150px; margin: auto;"></i>
+                    <div class="card-body">
                         <p class="card-text">CADASTROS</p>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-sm-12">
+            <div class="col-lg-4 col-md-5 col-sm-6 col-xs-12 button-container mb-4">
                 <div class="card" style="width: 18rem;">
-                    <i class="bi bi-tags" style="font-size: 100px; margin: auto;"></i>
-                    <div class="card-body" style="margin: auto">
+                    <i class="bi bi-tags" style="font-size: 150px; margin: auto;"></i>
+                    <div class="card-body">
                         <p class="card-text">VENDAS</p>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-6 col-sm-12">
-                <div class="button">ESTOQUE</div>
+            <div class="col-lg-4 col-md-5 col-sm-6 col-xs-12 button-container mb-4">
+                <div class="card" style="width: 18rem;">
+                    <i class="bi bi-tags" style="font-size: 150px; margin: auto;"></i>
+                    <div class="card-body">
+                        <p class="card-text">ESTOQUE</p>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-6 col-sm-12">
-                <div class="button">FATURAMENTO</div>
+            <div class="col-lg-4 col-md-5 col-sm-6 col-xs-12 button-container mb-4">
+                <div class="card" style="width: 18rem;">
+                    <i class="bi bi-tags" style="font-size: 150px; margin: auto;"></i>
+                    <div class="card-body">
+                        <p class="card-text">FATURAMENTO</p>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-6 col-sm-12">
-                <div class="button">ADMINISTRAÇÃO</div>
+            <div class="col-lg-4 col-md-5 col-sm-6 col-xs-12 button-container mb-4">
+                <div class="card" style="width: 18rem;">
+                    <i class="bi bi-tags" style="font-size: 150px; margin: auto;"></i>
+                    <div class="card-body">
+                        <p class="card-text">ADMINISTRAÇÃO</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+
+    <div class="container-fluid mt-3">
+
+    <div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>

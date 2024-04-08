61b96d452ecd45bd4b9b4968e1c8a955bef70064
devPxT <gabrielkauasantos11@gmail.com>
2024-04-08 07:44:54 -0300

    menu with hover and icons

--- index.php
+++ index.php
@@ -27,6 +27,13 @@
         border: 1px solid #ccc;
         cursor: pointer;
     }
+    .row a {
+        text-decoration: none;
+    }
+    .row .card:hover {
+        cursor: pointer;
+        background-color: #e6f4f7;
+    }
     .card-body {
         margin: 0px auto;
         padding: 0px;
@@ -46,85 +53,58 @@
 </style>
 </head>
 <body>
-    <nav class="navbar navbar-expand-md bg-body-tertiary" data-bs-theme="dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">SmartSales</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">SmartSales</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div class="offcanvas-body">
-                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Cadastros</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Vendas</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Estoque</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Faturamento</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="#">Administração</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </nav>
+    <?php require "geral/navbar.php" ?>
     
     <div class="container-fluid mt-3">
         <div class="row">
-            <div class="col-lg-4 col-md-5 col-sm-6 col-xs-12 button-container mb-4">
+            <a class="col-lg-4 col-md-5 col-sm-6 col-xs-12 button-container mb-4" href="#">
                 <div class="card" style="width: 18rem;">
-                    <i class="bi bi-box" style="font-size: 150px; margin: auto;"></i>
+                    <i class="bi bi-plus-square" style="font-size: 150px; margin: auto;"></i>
                     <div class="card-body">
                         <p class="card-text">CADASTROS</p>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-4 col-md-5 col-sm-6 col-xs-12 button-container mb-4">
+            </a>
+            <a class="col-lg-4 col-md-5 col-sm-6 col-xs-12 button-container mb-4" href="#">
                 <div class="card" style="width: 18rem;">
                     <i class="bi bi-tags" style="font-size: 150px; margin: auto;"></i>
                     <div class="card-body">
                         <p class="card-text">VENDAS</p>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-4 col-md-5 col-sm-6 col-xs-12 button-container mb-4">
+            </a>
+            <a class="col-lg-4 col-md-5 col-sm-6 col-xs-12 button-container mb-4" href="#">
                 <div class="card" style="width: 18rem;">
-                    <i class="bi bi-tags" style="font-size: 150px; margin: auto;"></i>
+                    <i class="bi bi-box" style="font-size: 150px; margin: auto;"></i>
                     <div class="card-body">
                         <p class="card-text">ESTOQUE</p>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-4 col-md-5 col-sm-6 col-xs-12 button-container mb-4">
+            </a>
+            <a class="col-lg-4 col-md-5 col-sm-6 col-xs-12 button-container mb-4" href="#">
                 <div class="card" style="width: 18rem;">
-                    <i class="bi bi-tags" style="font-size: 150px; margin: auto;"></i>
+                    <i class="bi bi-bar-chart" style="font-size: 150px; margin: auto;"></i>
                     <div class="card-body">
                         <p class="card-text">FATURAMENTO</p>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-4 col-md-5 col-sm-6 col-xs-12 button-container mb-4">
+            </a>
+            <a class="col-lg-4 col-md-5 col-sm-6 col-xs-12 button-container mb-4" href="#">
                 <div class="card" style="width: 18rem;">
-                    <i class="bi bi-tags" style="font-size: 150px; margin: auto;"></i>
+                    <i class="bi bi-building-gear" style="font-size: 150px; margin: auto;"></i>
                     <div class="card-body">
                         <p class="card-text">ADMINISTRAÇÃO</p>
                     </div>
                 </div>
-            </div>
+            </a>
+            <a class="col-lg-4 col-md-5 col-sm-6 col-xs-12 button-container mb-4" href="#">
+                <div class="card" style="width: 18rem;">
+                    <i class="bi bi-person" style="font-size: 150px; margin: auto;"></i>
+                    <div class="card-body">
+                        <p class="card-text">PERFIL</p>
+                    </div>
+                </div>
+            </a>
         </div>
     </div>
 

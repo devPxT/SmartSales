9d7679777763b06c5ec4b499500038b3e942e978
Bld01 <augusto.schwendtner@gmail.com>
2024-04-08 18:04:09 -0300

    footer

--- index.php
+++ index.php
@@ -63,6 +63,17 @@
         </div>
     </div>
 
+        <footer class="py-3 my-0" data-bs-theme="dark">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
+        </ul>
+        <p class="text-center text-body-secondary">© 2024 SmartSales</p>
+        </footer>
+
     <div class="container-fluid mt-3">
 
     <div>

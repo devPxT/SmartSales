86637d87577e9f93d832c0f23528ebab43f8e781
1Eduard0 <134875329+1Eduard0@users.noreply.github.com>
2024-04-08 14:42:42 -0300

    Create admin-produto.php

---
+++ admin-produto.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administração</title>
+    
+    <?php require "geral/links.php" ?>
+</head>
+<body id="admin">
+
+    <style>
+        .card-body {
+            text-align: center;
+        }
+        .card {
+            align-items: center;
+        }
+        .card img {
+            max-width: 450px;
+            /* margin: auto; */
+        }
+    </style>
+
+    <?php require "geral/navbar.php" ?>
+
+    <div class="container-fluid mt-3">
+        <div class="row row-cols-1 row-cols-lg-2 g-4 me-md-5 ms-md-5">
+            <div class="col">
+                <div class="card">
+                    <img src="icons/new-box.png" alt="CATEGORIAS">
+                    <div class="card-body">
+                        <!-- <i class="bi bi-box" style="font-size: 300px;"></i> -->
+                        <p class="card-text">CATEGORIAS</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="icons/acess.png" alt="PRODUTOS">
+                    <div class="card-body">
+                        <p class="card-text">PRODUTOS</p>
+                    </div>
+                </div>
+            </div>
+        <div>
+    </div>
+</body>
+</html>
